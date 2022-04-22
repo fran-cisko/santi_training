8b5a74a199276d3236d934aa0036d56714bba6ad
fran-cisko <franciscosciarretta@gmail.com>
2022-04-22 16:43:02 -0300

    homework exercise 1

---
+++ practice26_array.js
@@ -0,0 +1,13 @@
+//11.Hacer una función que reciba el array de países de áfrica y devuelva el nombre de todos los países que tienen el francés como idioma oficial(include)
+import africa from "./africa.js"
+
+const isFrenchOfficialLanguage = (country) => country.official_languages.include("French")
+
+const getCountryName = (country) => country.name
+
+const getCountriesFrenchOfficialLanguage = (countries)=> countries
+    .filter(isFrenchOfficialLanguage)
+    .map(getCountryName)
+
+
+console.log(getCountriesFrenchOfficialLanguage(africa))
